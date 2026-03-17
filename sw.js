60844049980120c6cb90b5327e649ce11629d5d8
Nestor Alvarado <nestor@Nestors-iMac.local>
2026-03-17 16:45:51 -0500

    Actualizando Service Worker para cache

--- sw.js
+++ sw.js
@@ -1,55 +1,16 @@
-const CACHE_NAME = 'tgrafico-cache-v1';
-const STATIC_ASSETS = [
-  '/',
-  '/index.html',
-  '/css/style.css',
-  '/css/bootstrap.css',
-  '/js/main.js',
-  '/js/bootstrap.bundle.js',
-  '/img/isotipo.svg',
-  '/manifest.json'
-];
+const CACHE_NAME = 'v1_cache_nestor_pwa';
+const urlsToCache = ['./', './index.html'];
 
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Opened cache');
-      return cache.addAll(STATIC_ASSETS);
-    })
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting())
   );
-  self.skipWaiting();
 });
 
-self.addEventListener('activate', (event) => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
-  );
-  self.clients.claim();
-});
-
-// Estrategia: Stale-While-Revalidate
-self.addEventListener('fetch', (event) => {
-  // Ignorar peticiones que no sean GET (como el envío de email)
-  if (event.request.method !== 'GET') return;
-
-  event.respondWith(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.match(event.request).then((cachedResponse) => {
-        const fetchPromise = fetch(event.request).then((networkResponse) => {
-          cache.put(event.request, networkResponse.clone());
-          return networkResponse;
-        });
-        return cachedResponse || fetchPromise;
-      });
-    })
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
